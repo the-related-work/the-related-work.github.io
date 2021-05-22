c3be00f75aa5c37a4e386429ba9dc9fb8052b8ef
yongchanghao <20069446+yongchanghao@users.noreply.github.com>
2021-05-22 23:55:15 +0800

    Init
    With 1 paper "Decoding with Value Networks for Neural Machine Translation"

---
+++ src/build.sh
@@ -0,0 +1,2 @@
+cd tex
+pandoc  --metadata-file ../metadata.yaml main.tex -s   -o ../../index.html -f latex -t html5  --citeproc --bibliography=lifetime-bib/lifetime.bib --bibliography=adhoc.bib  --biblatex --mathjax
