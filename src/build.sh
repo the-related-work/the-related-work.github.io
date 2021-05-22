679b516288ae702f728dce1c553d0cfc0920f1e7
yongchanghao <20069446+yongchanghao@users.noreply.github.com>
2021-05-23 01:16:10 +0800

    Change to GitHub CSS style
    Update style
    Hide references by default, click "show" button to display.

--- src/build.sh
+++ src/build.sh
@@ -1,2 +1,2 @@
 cd tex
-pandoc  --metadata-file ../metadata.yaml main.tex -s   -o ../../index.html -f latex -t html5  --citeproc --bibliography=lifetime-bib/lifetime.bib --bibliography=adhoc.bib  --biblatex --mathjax
+pandoc  --metadata-file ../metadata.yaml main.tex -s   -o ../../index.html -f latex -t html5  --citeproc --bibliography=lifetime-bib/lifetime.bib --bibliography=adhoc.bib  --biblatex --mathjax --template=github
